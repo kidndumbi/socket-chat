562a0e987e3fc49262520c0d0ac24d33cf926d4b
Cassava <kidndumbi@gmail.com>
2019-12-09 23:37:01 -0500

    test run

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+
+import * as path from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
+  @Get('size')
+  home() {
     return this.appService.getHello();
   }
 }
